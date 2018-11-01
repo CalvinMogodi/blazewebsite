79f343d935d752c33c6cbcb967b42401aa13d5cb
Calvin <cmogodi@gmail.com>
2018-11-01 23:28:46 +0200

    irst commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blazewebsite';
+  title = 'Blaze ';
+  public timer = 0;
+  public days = 0;
+  public hours = 0;
+  public minutes = 0;
+  public seconds = 0;
+  public showError = false;
+  public submitAttempt = false;
+  contactUsForm: FormGroup;
+  public message = {
+    name: '',
+    surname: '',
+    contactNumber: '',
+    emailaddress: '',
+    message: '',
+  }
+
+  constructor(public formBuilder: FormBuilder, public db: AngularFireDatabase) {
+    this.timerTick();
+    this.contactUsForm = formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      surname: ['', Validators.compose([Validators.required])],
+      contactNumber: ['', Validators.compose([Validators.required])],
+      emailaddress: ['', Validators.compose([Validators.required])],
+      message: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  sendMessage (){
+    this.submitAttempt = true;
+    this.showError = false;
+    if (this.contactUsForm.valid) {
+      this.db.list('/messages').push(this.message);  
+      let emailTosent = {
+        name: this.message.name,
+        surname: this.message.surname,
+        contactnumber: "0" + this.message.contactNumber,
+        emailaddress: this.message.emailaddress,
+        message: this.message.message,
+      };
+      let emailTosentJson = JSON.stringify(emailTosent);
+      this.showError = true;
+      this.message = {
+        name: '',
+        surname: '',
+        contactNumber: '',
+        emailaddress: '',
+        message: '',
+      }
+      this.contactUsForm.reset();
+      this.submitAttempt = false;
+    }
+  }
+
+  
+  timerTick() {
+    setTimeout(() => {
+      let toDate = new Date("2018/11/14 0:00 AM");
+      //let toDate = new Date("2018/10/0 15:4 PM");
+      var now = new Date();
+      var difference = toDate.getTime() - now.getTime();
+      if (difference <= 0 || isNaN(difference)) { return; }
+      var seconds = Math.floor(difference / 1000);
+      var minutes = Math.floor(seconds / 60);
+      var hours = Math.floor(minutes / 60);
+      var days = Math.floor(hours / 24);
+  
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+  
+      this.days = days;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+      if (difference > 0) {
+        this.timerTick();
+      }
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+var config = {
+  apiKey: "AIzaSyBVnK7mzG3o4pJjLoHexg8RJOdJj3J4cPc",
+  authDomain: "test-3bbd0.firebaseapp.com",
+  databaseURL: "https://test-3bbd0.firebaseio.com",
+  projectId: "test-3bbd0",
+  storageBucket: "test-3bbd0.appspot.com",
+  messagingSenderId: "27131835239"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule, // for database
   ],
   providers: [],
   bootstrap: [AppComponent]
